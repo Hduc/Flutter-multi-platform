190023fadd9070eb262467deb78be3b8dff5e53f
nhduc <nhduc@lacviet.com.vn>
2021-07-05 08:03:27 +0700

    Create Todo list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,56 @@
-import 'package:myapp/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Todo List"),
+        ),
+        body: TodoListContainer(),
+      ),
+    );
+  }
+}
+
+class TodoListContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _buildHeader(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    var txtTodoController = TextEditingController();
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: TextFormField(
+          controller: txtTodoController,
+          decoration:
+              InputDecoration(labelText: "Add Todo", hintText: "Add todo"),
+        )),
+        SizedBox(
+          width: 20,
+        ),
+        RaisedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.add),
+          label: Text('Add'),
+        )
+      ],
+    );
+  }
+}
--- lib/src/validators/validations.dart
+++
@@ -1,9 +0,0 @@
-class Validations {
-  static bool isValidUser(String user) {
-    return user != null && user.length > 6 && user.contains("@");
-  }
-
-  static bool isValidPass(String pass) {
-    return pass != null && pass.length > 6;
-  }
-}
